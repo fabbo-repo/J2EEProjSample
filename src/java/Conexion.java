bf7b1d49eb9cb4b1342f6996416f97d70c284a92
Wyuuri <maria01xyz@gmail.com>
2022-01-22 22:25:30 +0100

    conexion bd
    jbkj

---
+++ src/java/Conexion.java
@@ -0,0 +1,25 @@
+package java;
+import java.sql.*;
+
+public class Conexion {
+
+	public static void main(String[] args) {
+		try {
+			//1. CREAR CONEXION
+			Connection myConn = DriverManager.getConnection("jdbc:postgresql://192.168.0.35:5432/javadb1", "javauser1db", "j4a7vapqw3");
+			//2. CREAR OBJETO STATEMENT
+			Statement myState = myConn.createStatement();
+			//3. EJECUTAR SQL
+			ResultSet res = myState.executeQuery("SELECT * FROM PRODUCTOS");
+			//4. RECOGER EL RESULTSET
+			while(res.next()) {
+				System.out.println(res.getString("NOMBREARTICULO") + " " + res.getString("CODIGOARTICULO") + " " + res.getDouble("PRECIO")*2
+						+ " " + res.getDate("FECHA"));
+			}
+		}
+		catch (Exception e) {
+			System.out.println("NO CONECTA!!");
+			e.printStackTrace();
+		}
+	}
+}
